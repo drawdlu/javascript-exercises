856aed2ed52ac3467eab1091591efd544bed6e16
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-01 20:00:50 +0800

    06 exercise PASS all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!(year % 100)) {
+        if (!(year % 4) && !(year % 400)) {
+            return true;
+        }
+        return false;
+    } else if (!(year % 4)) {
+        return true;
+    }
+    
+    return false;
 };
 
 // Do not edit below this line
