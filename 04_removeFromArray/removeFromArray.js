82c744ae9af4b88271a019b3374e4ad3fda9f1fc
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-01 19:15:01 +0800

    04 exercise PASS multiple of same value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function(array, toRemoveFromArray) {
-    array.splice(array.indexOf(toRemoveFromArray), 1);
+const removeFromArray = function(array, ...toRemove) {
+    
+    toRemove.forEach( (remove) => {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === remove) {
+                array.splice(array.indexOf(remove), 1);
+                i--;
+            }
+        }
+    });
+    
+
     return array;
 };
 
