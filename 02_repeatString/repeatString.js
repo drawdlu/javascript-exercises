2c01b26e8c1fdb22fe050e9a7a65a6056e3af507
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-01 18:14:47 +0800

    02 exercise PASSED first 4 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(word, repeatWordNTimes) {
+    let finalString = '';
 
+    for (let i = 0; i < repeatWordNTimes; i++) {
+        finalString += word;
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
