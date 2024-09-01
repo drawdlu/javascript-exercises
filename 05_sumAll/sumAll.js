19aa538ed28865a62725c85261f871132b91bb0d
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-01 19:29:40 +0800

    05 exercise PASS first 2 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(sumFrom, sumTo) {
+    let sum = 0;
 
+    for (let i = sumFrom; i <= sumTo; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
