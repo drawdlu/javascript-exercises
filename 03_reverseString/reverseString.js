75cd0bde1a82117fbaa644e10fc5a876b6c657c2
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-01 18:28:17 +0800

    03 exercise PASS all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let reverseText = '';
+    let textArray = text.split('');
+    let textLength = textArray.length;
 
+    for (let i = textLength - 1; i >= 0; i--) {
+        reverseText += textArray[i];
+    }
+
+    return reverseText;
 };
+
+// create new string to hold reversed string
+
+// split text into an array 
+    // loop through array backwards
+    // concatenate each character into a new string
 
 // Do not edit below this line
 module.exports = reverseString;
