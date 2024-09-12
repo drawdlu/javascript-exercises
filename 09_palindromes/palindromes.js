f6356432c36df71ff6ef28225e5b264010a5311a
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-12 19:44:55 +0800

    09 exercise finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const palin = getLowerCharsAndReverse(word);
+    const stringLen = palin.word.length
+  
+    for (let i = 0; i < stringLen; i++) {
+        if (palin.word[i] !== palin.reverse[i]) {
+            return false;
+        }
+    }
+    return true;
 };
+
+function getLowerCharsAndReverse(chars) {
+    const wordChars = chars.toLowerCase().split('').filter( (item) => item.match(/[a-zA-Z0-9]/));
+    const word = wordChars
+    const reverse = wordChars.slice().reverse()
+    return {word: word, reverse: reverse, wordChars: wordChars}
+}
 
 // Do not edit below this line
 module.exports = palindromes;
