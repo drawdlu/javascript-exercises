c76552290bcb5abd5aac05ff7570f62f211f4019
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-12 20:21:53 +0800

    12 exercise finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce( (oldest, person) => {
+        let oldestDeathYear;
+        let personDeathYear;
+        
+        if (!oldest.yearOfDeath) {
+            oldestDeathYear = new Date().getFullYear();
+        } else {
+            oldestDeathYear = oldest.yearOfDeath;
+        }
 
+        if (!person.yearOfDeath) {
+            personDeathYear = new Date().getFullYear();
+        } else {
+            personDeathYear = person.yearOfDeath;
+        }
+
+        let oldestAge = oldestDeathYear - oldest.yearOfBirth;
+        let personAge = personDeathYear - person.yearOfBirth;
+
+        if (personAge > oldestAge) {
+            oldest = person;
+        }
+
+        return oldest;
+    })
 };
 
 // Do not edit below this line
